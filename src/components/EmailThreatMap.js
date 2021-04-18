0b04a8cc19fc6a1eb7c04c16636bf1d771b7f172
Will Lorand <wlorand@cisco.com>
2021-04-18 09:08:43 -0700

    feat: added lite theme map tiles

--- src/components/EmailThreatMap.js
+++ src/components/EmailThreatMap.js
@@ -21,9 +21,13 @@ function EmailThreatMap() {
   // prop vars
   const worldCenter = [22.6767777, -3.9824581]; // Mali
   const initialZoom = 2;
+  const liteTileUrl =
+    "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png";
+
   const darkTileUrl =
     "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png";
-  const darkTileAttr =
+
+  const stadiaTileAttr =
     '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 
   // const earthquakeIcon = new DivIcon({
@@ -51,7 +55,7 @@ function EmailThreatMap() {
         minZoom={initialZoom}
         scrollWheelZoom={false}
       >
-        <TileLayer url={darkTileUrl} attribution={darkTileAttr} />
+        <TileLayer url={liteTileUrl} attribution={stadiaTileAttr} />
         {/* <MarkerClusterGroup
           showCoverageOnHover={true}
           //    iconCreateFunction={createCustomClusterIcon}
